3a9395a00f3dfae07207f3b5bfba90137326e519
Shawn Berg <bergshawnm@gmail.com>
2023-12-21 11:10:46 -0600

    Initial commit

---
+++ folding_scheduler.sh
@@ -0,0 +1,7 @@
+#Check indoor temperature if possible else check outdoor temperature
+
+#If indoor temperature is below 68 degrees, run folding@home
+#Continuously check indoor temperature every 5 minutes?
+
+#If outdoor temperature is below 32 degrees, run folding@home
+#Continuously check indoor temperature every 5 minutes?
