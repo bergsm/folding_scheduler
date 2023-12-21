fdab52ac573944e1636abba7d945ed8f70368ddd
Shawn Berg <bergshawnm@gmail.com>
2023-12-21 11:19:03 -0600

    Added systemctl commands

--- folding_scheduler.sh
+++ folding_scheduler.sh
@@ -3,5 +3,10 @@
 #If indoor temperature is below 68 degrees, run folding@home
 #Continuously check indoor temperature every 5 minutes?
 
+systemctl start foldingathome.service
+systemctl start foldingathome-nvidia.service
+
 #If outdoor temperature is below 32 degrees, run folding@home
 #Continuously check indoor temperature every 5 minutes?
+systemctl start foldingathome.service
+systemctl start foldingathome-nvidia.service
